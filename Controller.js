6bab2c43b54acfff64a4c351b73994989ce36415
IvakhaBohdan <chesteemiller22@gmail.com>
2026-03-24 00:38:49 +0200

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -3,10 +3,16 @@ class AppController {
         this.model = model;
         this.view = view;
 
+        // 🔄 підписка на зміни даних
         this.model.bindDataChanged(() => this.updateView());
+
+        // перший рендер
         this.updateView();
 
+        // 🎯 обробники подій
         this.view.bindEvents({
+
+            // 📝 REGISTER
             register: (userData) => {
                 const res = this.model.registerUser(userData);
 
@@ -18,6 +24,7 @@ class AppController {
                 }
             },
 
+            // 🔐 LOGIN
             login: (email, password) => {
                 if (this.model.loginUser(email, password)) {
                     window.location.href = 'app.html';
@@ -26,38 +33,34 @@ class AppController {
                 }
             },
 
+            // 📝 ДОДАТИ ПОСТ
             addPost: (title, body) => {
                 this.model.addPost(title, body);
             },
 
+            // ❌ ВИДАЛИТИ ПОСТ
             deletePost: (id) => {
                 this.model.deletePost(id);
             },
 
+            // ❤️ ЛАЙК
             likePost: (id) => {
                 this.model.toggleLike(id);
             },
 
+            // 💬 ДОДАТИ КОМЕНТАР
             addComment: (postId, text) => {
                 this.model.addComment(postId, text);
+                this.updateView(); // одразу оновлюємо
+            },
+
+            // 🗑 ВИДАЛИТИ КОМЕНТАР
+            deleteComment: (postId, index) => {
+                this.model.deleteComment(postId, index);
                 this.updateView();
             },
 
-            deleteComment(postId, index) {
-                const post = this.posts.find(p => p.id === postId);
-                if (!post) return;
-
-                const comment = post.comments[index];
-
-                if (
-                    comment.author === this.currentUser.name ||
-                    post.author === this.currentUser.name
-                ) {
-                post.comments.splice(index, 1);
-                this.saveData();
-                    }
-                },
-
+            // 👤 ОНОВИТИ ПРОФІЛЬ
             updateProfile: (data) => {
                 this.model.updateUser(data);
                 this.updateView();
@@ -65,21 +68,24 @@ class AppController {
         });
     }
 
+    // 🔄 ОНОВЛЕННЯ UI
     updateView() {
         const user = this.model.currentUser;
 
         if (!user) return;
 
+        // 📰 Пости
         if (this.view.postsContainer) {
             this.view.displayPosts(this.model.posts, user.name);
         }
 
+        // 👤 Профіль
         if (this.view.profileName) {
             const count = this.model.posts.filter(
-            p => p.author === user.name
+                p => p.author === user.name
             ).length;
 
-        this.view.displayUserProfile(user, count);
+            this.view.displayUserProfile(user, count);
         }
     }
 }

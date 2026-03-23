1e8567806814dd559f59e5934553f26db1ecf3be
IvakhaBohdan <chesteemiller22@gmail.com>
2026-03-23 21:31:01 +0200

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -3,26 +3,56 @@ class AppController {
         this.model = model;
         this.view = view;
 
+        // Підписка на оновлення даних
         this.model.bindDataChanged(() => this.updateView());
+        
+        // Первинне відображення
         this.updateView();
 
+        // Налаштування подій
         this.view.bindEvents({
             addPost: (t, b) => this.model.addPost(t, b),
             deletePost: (id) => this.model.deletePost(id),
             likePost: (id) => this.model.toggleLike(id),
             addComment: (id, text) => this.model.addComment(id, text),
             updateProfile: (data) => this.model.updateUser(data),
-            register: (d) => this.model.registerUser(d).success ? window.location.href='login.html' : alert('Помилка'),
-            login: (e, p) => this.model.loginUser(e, p) ? window.location.href='app.html' : alert('Невірно')
+            
+            // ЛОГІКА РЕЄСТРАЦІЇ
+            register: (userData) => {
+                const result = this.model.registerUser(userData);
+                if (result.success) {
+                    alert('Реєстрація успішна!');
+                    // Після реєстрації автоматично логінимо і йдемо в профіль
+                    this.model.loginUser(userData.email, userData.password);
+                    window.location.href = 'profile.html'; 
+                } else {
+                    alert('Користувач з таким Email вже існує');
+                }
+            },
+
+            // ЛОГІКА ВХОДУ
+            login: (email, password) => {
+                if (this.model.loginUser(email, password)) {
+                    // Після успішного входу переходимо на головну сторінку блогу
+                    window.location.href = 'app.html';
+                } else {
+                    alert('Невірний email або пароль');
+                }
+            }
         });
     }
 
     updateView() {
-        const email = this.model.currentUser ? this.model.currentUser.email : null;
+        const currentUser = this.model.currentUser;
+        const email = currentUser ? currentUser.email : null;
+        
+        // Оновлюємо пости
         this.view.displayPosts(this.model.posts, email);
-        if (this.model.currentUser) {
-            const count = this.model.posts.filter(p => p.author === this.model.currentUser.name).length;
-            this.view.displayUserProfile(this.model.currentUser, count);
+        
+        // Оновлюємо дані в профілі, якщо ми на сторінці профілю
+        if (currentUser) {
+            const userPostsCount = this.model.posts.filter(p => p.author === currentUser.name).length;
+            this.view.displayUserProfile(currentUser, userPostsCount);
         }
     }
 }

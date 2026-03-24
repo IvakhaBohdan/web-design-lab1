a551e0b41936d5eac71e826ea7e75cdc6a9fbd9f
IvakhaBohdan <chesteemiller22@gmail.com>
2026-03-24 15:43:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const model = new AppModel();
     const view = new AppView();
-    new AppController(model, view);
+    const controller = new AppController(model, view);
 
-    const user = JSON.parse(localStorage.getItem('currentUser'));
+    const user = model.currentUser;
 
     const protectedPages = ['app.html', 'profile.html'];
     const page = window.location.pathname.split('/').pop();
 
+    //  захист сторінок
     if (protectedPages.includes(page) && !user) {
         window.location.href = 'login.html';
+        return;
+    }
+
+    if (document.getElementById('hero-action')) {
+        controller.initIndex();
     }
 });

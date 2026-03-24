33b9d9cf9b3bde508aa05ebef4cd7929e0603497
IvakhaBohdan <chesteemiller22@gmail.com>
2026-03-24 16:23:26 +0200

    Update Controller.js

--- Controller.js
+++ Controller.js
@@ -3,12 +3,16 @@ class AppController {
         this.model = model;
         this.view = view;
 
+        // Реєстрація оновлення інтерфейсу при зміні даних у моделі
         this.model.bindDataChanged(() => this.updateView());
 
+        // Первинне відображення даних
         this.updateView();
 
+        // Прив'язка дій користувача з View до методів Model
         this.view.bindEvents({
 
+            // Обробка реєстрації нового користувача
             register: (userData) => {
                 const res = this.model.registerUser(userData);
 
@@ -20,6 +24,7 @@ class AppController {
                 }
             },
 
+            // Обробка входу в систему
             login: ({ email, password }) => {
                 if (this.model.loginUser(email, password)) {
                     window.location.href = 'app.html';
@@ -28,61 +33,75 @@ class AppController {
                 }
             },
 
+            // Обробка виходу з акаунта
             logout: () => {
                 this.model.logoutUser();
                 window.location.href = 'login.html';
             },
 
+            // Ініціалізація відображення головної сторінки
             initIndex() {
                 const user = this.model.currentUser;
                 this.view.renderIndex(user);
             },
 
+            // Додавання нового поста
             addPost: (title, body) => {
                 this.model.addPost(title, body);
                 this.view.hidePostForm();
             },
 
+            // Керування видимістю форми створення поста
             togglePostForm: () => {
                 this.view.togglePostForm();
             },
 
+            // Приховування форми створення поста
             hidePostForm: () => {
                 this.view.hidePostForm();
             },
 
+            // Видалення поста через модель
             deletePost: (id) => {
                 this.model.deletePost(id);
             },
 
+            // Обробка лайка поста
             likePost: (id) => {
                 this.model.toggleLike(id);
             },
 
+            // Додавання коментаря до поста
             addComment: (postId, text) => {
                 this.model.addComment(postId, text);
             },
 
+            // Видалення коментаря через модель
             deleteComment: (postId, index) => {
                 this.model.deleteComment(postId, index);
             },
 
+            // Оновлення текстових даних профілю
             updateProfile: (data) => {
                 this.model.updateUser(data);
             },
 
+            // Оновлення зображення аватара
             updateAvatar: (base64) => {
                 this.model.updateAvatar(base64);
             },
 
+            // Пряме редагування поста
             editPost: (id, text) => {
                 this.model.editPost(id, text);
             },
 
+            // Пряме редагування коментаря
             editComment: (postId, index, text) => {
                 this.model.editComment(postId, index, text);
             },
 
+            // Запуск діалогу для редагування поста
             startEditPost: (id) => {
                 const newText = this.view.showPrompt('Новий текст поста:');
                 if (newText) {
@@ -90,6 +109,7 @@ class AppController {
                 }
             },
 
+            // Запуск діалогу для редагування коментаря
             startEditComment: (postId, index) => {
                 const newText = this.view.showPrompt('Новий коментар:');
                 if (newText) {
@@ -99,6 +119,7 @@ class AppController {
         });
     }
 
+    // Головний метод оновлення інтерфейсу: підготовка даних та виклик рендерінгу
     updateView() {
         const user = this.model.currentUser;
 
@@ -127,10 +148,12 @@ class AppController {
             };
         });
 
+        // Виклик відображення постів, якщо контейнер присутній на сторінці
         if (this.view.postsContainer) {
             this.view.displayPosts(postsForView);
         }
 
+        // Виклик відображення профілю та статистики
         if (this.view.profileName) {
             const stats = this.model.getUserStats();
             this.view.displayUserProfile(user, stats);
